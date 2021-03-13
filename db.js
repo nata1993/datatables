6dfe17f3cd2d1943fbbe84750a428a6c099cac48
kebo <bogdan.parubok@hotmail.com>
2021-03-14 00:04:12 +0200

    finished

--- db.js
+++ db.js
@@ -23,5 +23,41 @@ const getDB = () => {
     throw "No db found!";
 }
 
+const save = (name, position, office, extn, startDate, salary) => {
+    const db = getDB(); // connect to mongo db
+    let dbOperation;
+
+    dbOperation = db.collection('data').insertOne({
+        name : name,
+        position: position,
+        office : office,
+        extn: extn,
+        startDate: startDate,
+        salary: salary
+    });
+    
+
+    return dbOperation.then(result => {
+        console.log("success");
+    })
+    .catch(error => {
+        throw error;
+    });
+}
+
+const fetchAll = () => {
+    const db = getDB();
+
+    return db.collection("data").find().toArray()
+    .then(function(data) {
+        return data;
+    })
+    .catch(error=> {
+        console.log("Failed to fetch all data");
+    });
+}
+
 exports.mongoConnect = mongoConnect;
 exports.getDB = getDB;
+exports.fetchAll = fetchAll;
+exports.save = save;
--- server.js
+++ server.js
@@ -2,15 +2,39 @@ const express = require('express');
 const bParser = require('body-parser');
 const ejs = require('ejs');
 const mongoConnect = require('./db').mongoConnect;
+const db = require('./db');
 
 const port = 3000;
 
 const app = express();
+app.use(bParser.urlencoded({extended: true}));
 app.set('view engine', ejs);
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>{
-    res.render("index.ejs");
+    db.fetchAll().then(data =>{
+        res.render("index.ejs", { data : data });
+    });
+});
+
+app.post('/addData', (req, res) => {
+
+    if(req.body.name != "" &&
+        req.body.position != "" &&
+        req.body.office != "" &&
+        req.body.extn != "" &&
+        req.body.startDate != "" &&
+        req.body.salary != ""){
+            db.save(
+                req.body.name, 
+                req.body.position, 
+                req.body.office, 
+                req.body.extn, 
+                req.body.startDate, 
+                req.body.salary
+            );
+    }
+    res.redirect('/');
 });
 
 mongoConnect(() => {
